f69173a19df761b706bc475f1c7434eb3aa4be1d
Saran <mvsarankumar@gmail.com>
2025-08-19 14:33:55 +0530

    Initial commit - Applitools Visual Testing setup

---
+++ src/test/java/VisualTesting/Applitest.java
@@ -0,0 +1,34 @@
+package VisualTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.applitools.eyes.selenium.Eyes;
+
+public class Applitest {
+	
+	@Test
+	public void test() {
+		
+		WebDriver driver = new ChromeDriver();
+		//driver.get("https://applitools.com/helloworld/");
+		//driver.get("https://applitools.com/helloworld/?diff1");
+		driver.get("https://applitools.com/helloworld/?diff2");
+		
+		
+		Eyes eyes = new Eyes();
+		
+		eyes.setApiKey("Ra3BljCeHPA108Rwcvig5iYOvuC107zgjNKNat9AzxAwKnA110");
+		
+		eyes.open(driver, "Applitools Demo", "Hello World Test");
+		
+		eyes.checkWindow("Hello World");
+		
+		eyes.close();
+		
+		
+		
+	}
+
+}
